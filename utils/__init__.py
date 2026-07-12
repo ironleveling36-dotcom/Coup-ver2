7e0f057ee4e5fe4ec1a826c7bce9d255539e0ea5
ironleveling36-dotcom <ironleveling36@gmail.com>
2026-07-12 19:59:19 +0530

    Add files via upload

---
+++ utils/__init__.py
@@ -0,0 +1,26 @@
+from .helpers import (
+    # roles
+    ROLE_SUPER_ADMIN, ROLE_ADMIN, ROLE_SUPPORT, ROLE_USER,
+    PERMISSIONS, env_role, is_admin, is_super_admin, role_can, role_label,
+    # ids / codes
+    generate_order_id, generate_ref_code,
+    # formatting
+    format_currency, chunks, safe_int, safe_float, valid_txn_id, fmt_dt,
+    format_delivery,
+    # discount
+    best_tier, calc_pricing, tiers_summary,
+    # upi qr
+    upi_link, make_upi_qr,
+)
+from . import animations
+
+__all__ = [
+    "ROLE_SUPER_ADMIN", "ROLE_ADMIN", "ROLE_SUPPORT", "ROLE_USER",
+    "PERMISSIONS", "env_role", "is_admin", "is_super_admin", "role_can", "role_label",
+    "generate_order_id", "generate_ref_code",
+    "format_currency", "chunks", "safe_int", "safe_float", "valid_txn_id",
+    "fmt_dt", "format_delivery",
+    "best_tier", "calc_pricing", "tiers_summary",
+    "upi_link", "make_upi_qr",
+    "animations",
+]
